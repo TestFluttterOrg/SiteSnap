608d87864f6097d9cba3fe57ef621c9d84a1721d
jmguillen1996 <josefmguillen@gmail.com>
2024-10-19 01:38:04 +0800

    Created icon widget.

--- lib/features/presentation/screen/user/user_screen.dart
+++ lib/features/presentation/screen/user/user_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sitesnap/core/constants/app_assets.dart';
 import 'package:sitesnap/features/presentation/components/app_scaffold.dart';
 import 'package:sitesnap/core/di/dependency_injection.dart' as di;
 import 'package:sitesnap/features/presentation/screen/user/bloc/user_bloc.dart';
-import 'package:sitesnap/main.dart';
 
 class UserScreen extends StatelessWidget {
   const UserScreen({super.key});
@@ -30,9 +30,9 @@ class _UserForm extends StatelessWidget {
       body: Container(
         padding: EdgeInsets.symmetric(
           vertical: 20.h,
-          horizontal: 20.w,
+          horizontal: 60.w,
         ),
-        child: Column(
+        child: const Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
@@ -49,11 +49,49 @@ class _IconView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-
-      ],
+    return Container(
+      color: Colors.transparent,
+      width: double.infinity,
+      height: 120.h,
+      child: const Stack(
+        alignment: Alignment.center,
+        children: [
+          Positioned(
+            left: 0,
+            top: 0,
+            child: _IconWidget(iconString: AppAssets.youtube),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: _IconWidget(iconString: AppAssets.spotify),
+          ),
+          Positioned(
+            right: 0,
+            bottom: 0,
+            child: _IconWidget(iconString: AppAssets.facebook),
+          ),
+        ],
+      ),
     );
   }
 }
 
+class _IconWidget extends StatelessWidget {
+  final String iconString;
+
+  const _IconWidget({
+    required this.iconString,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10.r),
+      child: Image.asset(
+        iconString,
+        width: 80.h,
+        height: 80.h,
+      ),
+    );
+  }
+}

38198510c1501ac10843d9fb9d482ca7824b865c
jmguillen1996 <josefmguillen@gmail.com>
2024-10-19 00:46:31 +0800

    created scaffold component.

--- lib/features/presentation/screen/user/user_screen.dart
+++ lib/features/presentation/screen/user/user_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:sitesnap/features/presentation/components/app_scaffold.dart';
 
 class UserScreen extends StatelessWidget {
   const UserScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return AppScaffold(
+      backgroundColor: Colors.white,
+      body: Container(),
+    );
   }
 }

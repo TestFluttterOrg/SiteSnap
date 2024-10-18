9b33fb8974620d04a43ec56c5c82ad57740f7947
jmguillen1996 <josefmguillen@gmail.com>
2024-10-19 01:09:55 +0800

    Added assets.

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,10 @@
+class AppAssets {
+  AppAssets._();
+
+  static const apple = "assets/images/apple.png";
+  static const facebook = "assets/images/facebook.png";
+  static const samsung = "assets/images/samsung.png";
+  static const spotify = "assets/images/spotify.png";
+  static const windows = "assets/images/windows.png";
+  static const youtube = "assets/images/youtube.png";
+}
--- lib/features/presentation/screen/user/user_screen.dart
+++ lib/features/presentation/screen/user/user_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sitesnap/features/presentation/components/app_scaffold.dart';
 import 'package:sitesnap/core/di/dependency_injection.dart' as di;
 import 'package:sitesnap/features/presentation/screen/user/bloc/user_bloc.dart';
+import 'package:sitesnap/main.dart';
 
 class UserScreen extends StatelessWidget {
   const UserScreen({super.key});
@@ -25,7 +27,33 @@ class _UserForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return AppScaffold(
       backgroundColor: Colors.white,
-      body: Container(),
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          vertical: 20.h,
+          horizontal: 20.w,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _IconView(),
+          ],
+        ),
+      ),
     );
   }
 }
+
+class _IconView extends StatelessWidget {
+  const _IconView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+
+      ],
+    );
+  }
+}
+
